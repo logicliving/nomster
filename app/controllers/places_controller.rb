c12636ffc92378d9d2ae5c6e2c5dce0317b8338c
Paul Jackow <paulj@logicliving.com>
2016-02-17 04:25:23 +0000

    Changed Entries per page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
   
   def index
   	#@places = Place.all
-    @places = Place.paginate(:page => params[:page], :per_page => 5)
+    @places = Place.paginate(:page => params[:page], :per_page => 4)
   end
 
   def new
